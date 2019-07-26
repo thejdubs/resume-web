caf772df936aa2db6ae210e2e9e21280c1410f57
thejdubs <thejdubs2@aol.com>
2019-07-26 05:26:58 -0400

    init commit

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+function Footer()
+{
+    document.write("<div class=footer>2019 John Wilson</div>")
+}
+
+	
+	
+	
+	
+	
+	
---
+++ js/navbar.js
@@ -0,0 +1,18 @@
+function NavigationBar()
+{
+    document.write("<div class = header>")
+	document.write("	<a class = name>John J. Wilson</a>")
+	document.write("	<a class = headerElement HREF=\"hobbies.html\">HOBBIES</a>")
+	document.write("	<a class = headerElement HREF=\"projects.html\">PROJECTS</a>")
+	document.write("	<a class = headerElement HREF=\"education.html\">EDUCATION</a>")	
+	document.write("	<a class = headerElement HREF=\"experience.html\">EXPERIENCE</a>")
+	document.write("	<a class = headerElement HREF=\"home.html\">HOME</a>")
+	document.write("</div><br><br><br><br>")
+}
+
+	
+	
+	
+	
+	
+	
